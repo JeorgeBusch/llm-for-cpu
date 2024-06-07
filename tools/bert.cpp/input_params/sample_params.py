c02a03421ecff78c31e551846c8088d54a3826f0
Avery Johnson <aej455@gmail.com>
2024-06-06 17:35:20 -0700

    add parameter sampling, ignore syscalls 38 and 36, update .gitignore

---
+++ tools/bert.cpp/input_params/sample_params.py
@@ -0,0 +1,19 @@
+import random 
+import time
+import sys
+import os
+
+random.seed(time.clock())
+
+try:
+    os.makedirs("batches")
+except OSError as exception:
+    pass
+
+with open("params", 'r') as f:
+    lines = f.readlines()
+    for i in range(int(sys.argv[1])):
+        rand_lines = random.sample(lines, int(sys.argv[2]))
+        with open("batches/params_" + str(int(sys.argv[2])%10)+"K" + "_" + str(i), "w") as out:
+            out.write("".join(line for line in rand_lines))
+    
