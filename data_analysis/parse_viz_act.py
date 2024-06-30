568625f73810929ac5e6df1185507316228cf6f3
pixelated-explorer <rm143@tamu.edu>
2024-06-30 01:36:08 -0500

    even more changes

--- data_analysis/parse_viz_act.py
+++ data_analysis/parse_viz_act.py
@@ -78,85 +78,6 @@ df = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in combined_data.items()]))
 df.head(100000)
 
 #%%
-'''
-import pandas as pd
-import seaborn as sns
-import re
-import os
-
-# standard count of files in the folder. 
-def count_files_in_directory(directory_path):
-    try:
-        # List all the files and directories in the given directory
-        items = os.listdir(directory_path)
-
-        # Filter out directories, keeping only files
-        folders = [item for item in items if os.path.isdir(os.path.join(directory_path, item))]
-        
-        # Return the number of files
-        return len(folders)
-    
-    except FileNotFoundError:
-        return 0
-    except Exception as e:
-        return 0
-
-
-# The function needs to be used two times: once to parse the outer layer of param files, and once 
-# to parse the inner layer. 
-
-# First order of the function. 
-directory_path = 'C:\stats_research\llm-for-cpu\data_analysis\params'
-file_count = count_files_in_directory(directory_path)
-
-print(f"The file count of the exterior is  {file_count}")
-
-#-----------------------------------------------------------
-
-# Dictionary to hold the data
-data = {}
-
-
-for i in range(0, file_count):
-    file_path = f'C:\stats_research\llm-for-cpu\data_analysis\params_g/params_100k_{i}/stats.txt'
-    try:
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-
-        # Grabbing keys to use in the dictionary
-        for line in lines:
-            parts = line.split()
-            
-            if len(parts) < 2:
-                continue
-            
-            key = parts[0]
-            
-            # This giant if statment checks if there is an instruction or some other specific 
-            # columns for the new variables that'll be calculated later on. 
-            # This variable list may change depending on what we're looking at (including if we just want to focus on DRAM for instance)
-            # "data" in key) or ("Data" in key)
-
-            if ("inst" in key) or ("Inst" in key) or ('system.cpu.mmu.dtb.rdMisses' in key) or ('system.cpu.mmu.dtb.wrMisses' in key) or ('system.cpu.mmu.itb.rdMisses' in key) or ('system.cpu.mmu.itb.wrMisses' in key) or ('system.cpu.numCycles' in key):
-                if not("system.mem_ctrl.dram" in key):
-                    value = float(parts[1])
-
-                    if key in data:
-                        data[key].append(value)
-                    else:
-                        data[key] = [value]
-
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
-    except Exception as e:
-        print(f"An error occurred while reading {file_path}: {e}")
-
-df = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in data.items()]))
-
-print(df.head())
-'''
-
-#%%
 # # because there is no need for more padding (seems like every value is covered), check for missing values and remove/fill them?
 # df.fillna(0, inplace=True)
 # # df = df.drop(df.columns[0], axis=1)
@@ -180,64 +101,6 @@ df['IPC'] = df['simInsts'] / df['system.cpu.numCycles']
 
 df.to_csv("review.csv", sep=',', index=False)
 df.head()
-
-#%%
-
-# most important graphs are here regarding those new variables.
-# I think kde plots are cool for showing concentrations but idk how they'll be at the moment. 
-'''
-columns = ['data_mpki', 'inst_mpki', 'IPC']
-for column in columns:
-    plt.figure()  
-    sns.barplot(x=df.index, y=df[column], color='blue', label = 'Barplot', alpha=0.2)
-    sns.lineplot(x=df.index, y=df[column], marker='o', color = 'purple', label = 'Line')
-    plt.title(column)  
-    plt.xlabel('Index')  
-    plt.ylabel('Values')  
-    plt.tight_layout()
-    plt.show()  
-'''
-
-# %% 
-'''
-# Zoomed in plots like last time.
-
-fig, axes = plt.subplots(nrows = 1, ncols = 3, figsize = (18,5))
-
-# ------------- data_mpki -------------
-
-# sns.barplot(ax=axes[0], x = df.index, y = df['data_mpki'], color = 'blue', label = 'Barplot', alpha = 0.2)
-sns.scatterplot(ax = axes[0], x=df.index, y = df['data_mpki'], marker = 'o', color = 'purple', label = 'Line')
-axes[0].set_ylim(1.6398e6, 1.645e6)
-axes[0].set_title('data_mpki')
-axes[0].set_xlabel('Index')
-axes[0].set_ylabel('Values')
-
-# ------------- inst_mpki -------------
-
-plt.figure()
-# sns.barplot(ax=axes[1], x = df.index, y = df['inst_mpki'], color = 'blue', label = 'Barplot', alpha = 0.2)
-sns.scatterplot(ax = axes[1], x=df.index, y = df['inst_mpki'], marker = 'o', color = 'purple', label = 'Line')
-axes[1].set_ylim(0.0013050, 0.00132)
-axes[1].set_title('inst_mpki')
-axes[1].set_xlabel('Index')
-axes[1].set_ylabel('Values')
-
-# ------------- IPC -------------
-
-plt.figure()
-# sns.barplot(ax=axes[2], x = df.index, y = df['IPC'], color = 'blue', label = 'Barplot', alpha = 0.2)
-sns.scatterplot(ax=axes[2], x=df.index, y = df['IPC'], marker = 'o', color = 'purple', label = 'Line')
-axes[2].set_ylim(0.01200, 0.01204)
-axes[2].set_title('IPC')
-axes[2].set_xlabel('Index')
-axes[2].set_ylabel('Values')
-
-# adjusting the main layout
-plt.tight_layout()
-plt.show()
-
-'''
 
 # %%
 
@@ -329,8 +192,8 @@ fig1.show()
 fig2.show()
 
 # for streamlit: 
-st.plotly_chart(fig1)
-st.plotly_chart(fig2)
+# st.plotly_chart(fig1)
+# st.plotly_chart(fig2)
 
 # %%
 
@@ -385,9 +248,9 @@ fig_inst_mpki.show()
 fig_IPC.show()
 
 # for streamlit: 
-st.plotly_chart(fig_data_mpki)
-st.plotly_chart(fig_inst_mpki)
-st.plotly_chart(fig_IPC)
+# st.plotly_chart(fig_data_mpki)
+# st.plotly_chart(fig_inst_mpki)
+# st.plotly_chart(fig_IPC)
 
 # %% 
 
@@ -430,20 +293,18 @@ fig_data_mpki = px.density_heatmap(df, x=df.index, y='data_mpki', title='data_mp
 fig_inst_mpki = px.density_heatmap(df, x=df.index, y='inst_mpki', title='inst_mpki', labels={'x': 'Index', 'inst_mpki': 'Values'}, color_continuous_scale='Viridis')
 fig_IPC = px.density_heatmap(df, x=df.index, y='IPC', title='IPC', labels={'x': 'Index', 'IPC': 'Values'}, color_continuous_scale='Viridis')
 
-# Adjusting the layout
 fig_data_mpki.update_layout(width=600, height=400)
 fig_inst_mpki.update_layout(width=600, height=400)
 fig_IPC.update_layout(width=600, height=400)
 
-# Display the figures
 fig_data_mpki.show()
 fig_inst_mpki.show()
 fig_IPC.show()
 
 # for streamlit: 
-st.plotly_chart(fig_data_mpki)
-st.plotly_chart(fig_inst_mpki)
-st.plotly_chart(fig_IPC)
+# st.plotly_chart(fig_data_mpki)
+# st.plotly_chart(fig_inst_mpki)
+# st.plotly_chart(fig_IPC)
 
 #%%
 # before looking at the graph for each simulation, average each of the instructions 
@@ -514,6 +375,7 @@ plt.show()
 
 # HEATMAP AGAINST ONLY THESE CATEGORIES
 
+
 # Assuming df has 77 columns plus data_mpki, inst_mpki, and IPC
 # Find all columns in the DataFrame excluding 'data_mpki', 'inst_mpki', and 'IPC'
 categories = df.columns.difference(['data_mpki', 'inst_mpki', 'IPC'])
@@ -535,6 +397,7 @@ plt.subplot(3, 1, 2)
 sns.heatmap(pd.DataFrame(corr_matrix_inst_mpki, columns=['Correlation with inst_mpki']), annot=True, cmap='coolwarm')
 
 # Plot heatmap for IPC correlation
+st.subheader('Correlation with IPC')
 plt.subplot(3, 1, 3)
 sns.heatmap(pd.DataFrame(corr_matrix_ipc, columns=['Correlation with IPC']), annot=True, cmap='coolwarm')
 
