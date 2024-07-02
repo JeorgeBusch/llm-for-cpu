585eb2418cffd5c17d0287c8d012fdb235e19a50
pixelated-explorer <rm143@tamu.edu>
2024-07-02 15:17:18 -0500

    made new file

---
+++ data_analysis/ml_experiments.py
@@ -0,0 +1,16 @@
+# basics 
+import pandas as pd
+import numpy as np 
+
+# plotting and regular expressions (if we need for some reason)
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+import plotly.express as px
+import re
+
+# machine learning libraries -- 
+
+
+
+
