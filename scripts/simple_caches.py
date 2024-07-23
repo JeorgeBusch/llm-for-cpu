424e29234972344765269c926a6a2a895f2ac226
Spencer Banasik <36858080+smbanasik@users.noreply.github.com>
2024-07-23 16:47:31 -0500

    Add files via upload

---
+++ scripts/simple_caches.py
@@ -0,0 +1,92 @@
+import m5
+from m5.objects import *
+from caches import *
+import pandas as pd
+from gem5.components.processors.simple_switchable_processor import SimpleSwitchableProcessor
+from gem5.components.processors.cpu_types import CPUTypes
+
+system = System()
+
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = '1GHz'
+system.clk_domain.voltage_domain = VoltageDomain()
+
+system.mem_ranges = [AddrRange('512MB')]
+
+system.cpu = AtomicSimpleCPU()
+system.cpu.icache = L1ICache()
+system.cpu.dcache = L1DCache()
+
+system.membus = SystemXBar()
+system.l2bus = L2XBar()
+system.l2cache = L2Cache()
+
+system.cpu.icache.connectCPU(system.cpu)
+system.cpu.dcache.connectCPU(system.cpu)
+
+system.cpu.icache.connectBus(system.l2bus)
+system.cpu.dcache.connectBus(system.l2bus)
+
+# Example for cache replacement, default is LRUP, refer to caches.py for more info
+system.cpu.icache.replacement_policy = LRUP()
+system.cpu.dcache.replacement_policy = LRUP()
+system.l2cache.replacement_policy = LRUP()
+
+system.l2cache.connectCPUSideBus(system.l2bus)
+system.l2cache.connectMemSideBus(system.membus)
+
+
+
+system.cpu.createInterruptController()
+system.cpu.interrupts[0].pio = system.membus.mem_side_ports
+system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
+system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
+system.system_port = system.membus.cpu_side_ports
+
+system.mem_ctrl = MemCtrl()
+system.mem_ctrl.dram = DDR3_1600_8x8()
+system.mem_ctrl.dram.range = system.mem_ranges[0]
+system.mem_ctrl.port = system.membus.mem_side_ports
+
+
+binary = '/scratch/user/smb200007/research/llm-for-cpu/tools/bert.cpp/build/bin/main'
+
+# for gem5 V21 and beyond
+system.workload = SEWorkload.init_compatible(binary)
+
+process = Process()
+process.cmd = [binary]
+system.cpu.workload = process
+system.cpu.createThreads()
+
+system.switch_cpu = TimingSimpleCPU(switched_out=True, cpu_id=0)
+
+system.switch_cpu.workload = process
+system.switch_cpu.createThreads()
+switch_cpu_list = [(system.cpu, system.switch_cpu)]
+switch_cpu_list2 = [(system.switch_cpu, system.cpu)]
+
+root = Root(full_system = False, system = system)
+
+m5.instantiate()
+
+
+print("Beginning simulation!")
+exit_event = None
+switched = False
+
+while exit_event is None:
+    exit_event = m5.simulate()
+    
+    if exit_event.getCause() == "switchcpu":
+        print("Switching CPU...")
+        if not switched:
+            m5.switchCpus(root.system, switch_cpu_list)
+            switched = True
+        else:
+            m5.switchCpus(root.system, switch_cpu_list2)
+            switched = False
+        exit_event = None
+
+print('Exiting @ tick {} because {}'
+      .format(m5.curTick(), exit_event.getCause()))
