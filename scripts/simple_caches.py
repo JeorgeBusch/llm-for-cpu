3f329bca05175fd03ba2b18f5456919ee4dca24c
Spencer Banasik <36858080+smbanasik@users.noreply.github.com>
2024-07-24 12:41:40 -0500

    Added command line functionality to options

--- scripts/simple_caches.py
+++ scripts/simple_caches.py
@@ -1,9 +1,21 @@
 import m5
 from m5.objects import *
 from caches import *
+import argparse
 import pandas as pd
 from gem5.components.processors.simple_switchable_processor import SimpleSwitchableProcessor
 from gem5.components.processors.cpu_types import CPUTypes
+
+parser = argparse.ArgumentParser(description="Simple 2-level cache system.", epilog="Cache options may be found in src/mem/cache/Cache.py.\nReplacement options may be found in src/mem/cache/ReplacementPolicies.py")
+parser.add_argument("--mem_size", help=f"Memory size. Default: 512MB.")
+parser.add_argument("--l1i_size", help=f"L1 instruction cache size. Default: 16kB.")
+parser.add_argument("--l1d_size", help=f"L1 data cache size. Default: 64kB.")
+parser.add_argument("--l2_size", help=f"L2 cache size. Default: 256kB.")
+parser.add_argument("--l1i_replpolicy", help=f"L1 instruction replacement policy. Default: LRURP")
+parser.add_argument("--l1d_replpolicy", help=f"L1 data replacement policy. Default: LRURP")
+parser.add_argument("--l2_replpolicy", help=f"L2 replacement policy. Default: LRURP")
+
+options = parser.parse_args()
 
 system = System()
 
@@ -11,15 +23,19 @@ system.clk_domain = SrcClockDomain()
 system.clk_domain.clock = '1GHz'
 system.clk_domain.voltage_domain = VoltageDomain()
 
-system.mem_ranges = [AddrRange('512MB')]
+memSize = '512MB'
+if options.mem_size:
+    memSize = options.mem_size
+
+system.mem_ranges = [AddrRange(memSize)]
 
 system.cpu = AtomicSimpleCPU()
-system.cpu.icache = L1ICache()
-system.cpu.dcache = L1DCache()
+system.cpu.icache = L1ICache(options)
+system.cpu.dcache = L1DCache(options)
 
 system.membus = SystemXBar()
 system.l2bus = L2XBar()
-system.l2cache = L2Cache()
+system.l2cache = L2Cache(options)
 
 system.cpu.icache.connectCPU(system.cpu)
 system.cpu.dcache.connectCPU(system.cpu)
@@ -27,14 +43,8 @@ system.cpu.dcache.connectCPU(system.cpu)
 system.cpu.icache.connectBus(system.l2bus)
 system.cpu.dcache.connectBus(system.l2bus)
 
-# Example for cache replacement, default is LRUP, refer to caches.py for more info
-system.cpu.icache.replacement_policy = LRUP()
-system.cpu.dcache.replacement_policy = LRUP()
-system.l2cache.replacement_policy = LRUP()
-
 system.l2cache.connectCPUSideBus(system.l2bus)
 system.l2cache.connectMemSideBus(system.membus)
-
 
 
 system.cpu.createInterruptController()
