bc60fdb97fcb13afed4b92dec6bd3e1398a08bf3
Avery Johnson <aej455@gmail.com>
2024-10-14 17:24:43 -0500

    add llama framework

---
+++ tools/llama.cpp/common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 3852;
+char const *LLAMA_COMMIT = "511636d";
+char const *LLAMA_COMPILER = "x86_64-conda-linux-gnu-cc (Anaconda gcc) 11.2.0";
+char const *LLAMA_BUILD_TARGET = "x86_64-conda-linux-gnu";
